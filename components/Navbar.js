de14835c546b8fae913cb842db09618cbd6ee209
Dredeco <andrefersouza@gmail.com>
2023-05-04 18:58:20 -0300

    atualização da hero section e demais ajustes

---
+++ components/Navbar.js
@@ -0,0 +1,58 @@
+import React, {useEffect} from 'react'
+import Image from 'next/legacy/image'
+import Link from 'next/link'
+
+import Logo from './../public/images/logo.png'
+
+export default function Navbar() {
+
+    useEffect(() => {
+        let btns = document.getElementsByClassName('nav-link');
+
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', function(){
+                let current = document.getElementsByClassName('active');
+                current[0].className = current[0].className.replace(' active', '');
+                this.className += ' active'
+            })
+        }
+
+        console.log(btns);
+    }, 2000)
+
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <Link class="navbar-brand" href="/">
+                <Image 
+                    className='logo'
+                    src={Logo}
+                    width='180'
+                    height='60'
+                />
+            </Link>
+
+            <button 
+                class="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <Link class="nav-link active" aria-current="page" href="/">Início</Link>
+                    <Link class="nav-link" href="#about">Sobre</Link>
+                    <Link class="nav-link" href="#products">salas</Link>
+                    <Link class="nav-link" href="#footer">Contato</Link>
+                </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
---
+++ components/Products.js
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import React from 'react'
+
+import Reuniao from './../public/images/reuniao1.jpeg'
+import Compartilhada from './../public/images/estacao2.png'
+import BemEstar from './../public/images/bemestar.jpeg'
+import Privativa from './../public/images/privativa.jpeg'
+import PrivativaPremium from './../public/images/privativap.jpeg'
+import Ripada from './../public/images/ripada.jpeg'
+import Link from 'next/link'
+
+export default function Products() {
+  let salas = [
+    {name: 'Sala de Reunião', image: Reuniao, hora: "R$45,00", pacote: "R$400,00", diaria: "R$270,00", mensal: "Indisponível"},
+    {name: 'Sala Compartilhada', image: Compartilhada, hora: "Indisponível", pacote: "Indisponível", diaria: "R$45,00", mensal: "R$360,00"},
+    {name: 'Sala de Atendimento Bem-Estar', image: BemEstar, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
+    {name: 'Sala de Atendimento Privativa', image: Privativa, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
+    {name: 'Sala de Atendimento Porta Ripada', image: Ripada, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
+    {name: 'Sala Privativa Premium', image: PrivativaPremium, hora: "R$40,00", pacote: "R$350,00", diaria: "Indisponível", mensal: "Indisponível"},
+  ]
+
+  return (
+    <div id='products' className='products'>
+      <div className='container'>
+        <h1>Salas</h1>
+        <div className='products-wrapper'>
+          {salas.map((sala) => (
+            <div className='product-card'>
+              <Image
+                src={sala.image}
+                width={300}
+                height={400}
+              />
+              <div className='product-description'>
+                <h2>{sala.name}</h2>
+                <h4>Valores</h4>
+                <span>Hora: {sala.hora}</span>
+                <span>Pacote 10h: {sala.pacote}</span>
+                <span>Diária: {sala.diaria}</span>
+                <span>Mensal: {sala.mensal}</span>
+                <a href='https://wa.me/5522998727548' target='_blank'>Reserve agora</a>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Link href='https://trinitycoworking.github.io/Trinity_Horarios/' target='_blank' className='cta'><p>Consulte aqui a disponibilidade das salas</p></Link>
+      </div>
+    </div>
+  )
+}
